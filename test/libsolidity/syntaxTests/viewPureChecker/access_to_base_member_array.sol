0d853c037d3c10df25acd626761a6dcbfa92f816
Erik Kundt <erik@ethereum.org>
2019-09-18 15:36:45 +0200

    Adjusts syntax tests to read-only array length.

--- test/libsolidity/syntaxTests/viewPureChecker/access_to_base_member_array.sol
+++ test/libsolidity/syntaxTests/viewPureChecker/access_to_base_member_array.sol
@@ -3,9 +3,6 @@ contract A {
 }
 
 contract B is A {
-    function f() public view {
-        A.x.length = 2;
-    }
     function g() public pure returns (uint) {
         return A.x.length;
     }
@@ -14,8 +11,7 @@ contract B is A {
     }
 }
 // ----
-// TypeError: (87-97): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
-// TypeError: (170-173): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
-// TypeError: (170-180): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
-// TypeError: (249-252): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
-// TypeError: (249-255): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
+// TypeError: (109-112): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
+// TypeError: (109-119): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
+// TypeError: (188-191): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
+// TypeError: (188-194): Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
