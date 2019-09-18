9271a254591f23f1de0c2da9d037e36a3bbd2a2f
Erik Kundt <erik@ethereum.org>
2019-09-18 15:38:38 +0200

    Adjusts semantic tests to read-only array length.

--- test/libsolidity/semanticTests/viaYul/array_storage_index_access.sol
+++ test/libsolidity/semanticTests/viaYul/array_storage_index_access.sol
@@ -1,18 +1,15 @@
 contract C {
-	uint[] storageArray;
-	function test_indicies(uint256 len) public
-	{
-		storageArray.length = len;
+    uint[] storageArray;
+    function test_indicies(uint256 len) public
+    {
+        storageArray = new uint[](len);
+        for (uint i = 0; i < len; i++)
+            storageArray[i] = i + 1;
 
-		for (uint i = 0; i < len; i++)
-			storageArray[i] = i + 1;
-
-		for (uint i = 0; i < len; i++)
-			require(storageArray[i] == i + 1);
-	}
+        for (uint i = 0; i < len; i++)
+            require(storageArray[i] == i + 1);
+    }
 }
-// ====
-// compileViaYul: true
 // ----
 // test_indicies(uint256): 1 ->
 // test_indicies(uint256): 129 ->
--- test/libsolidity/semanticTests/viaYul/array_storage_length_access.sol
+++ test/libsolidity/semanticTests/viaYul/array_storage_length_access.sol
@@ -1,14 +1,13 @@
 contract C {
-	uint[] storageArray;
-	function set_get_length(uint256 len) public returns (uint256)
-	{
-		storageArray.length = len;
-		return storageArray.length;
-	}
-
+    uint[] storageArray;
+    function set_get_length(uint256 len) public returns (uint256)
+    {
+        storageArray = new uint[](len);
+        return storageArray.length;
+    }
 }
 // ====
-// compileViaYul: true
+// EVMVersion: >=petersburg
 // ----
 // set_get_length(uint256): 0 -> 0
 // set_get_length(uint256): 1 -> 1
@@ -16,4 +15,5 @@ contract C {
 // set_get_length(uint256): 20 -> 20
 // set_get_length(uint256): 0 -> 0
 // set_get_length(uint256): 0xFF -> 0xFF
-// set_get_length(uint256): 0xFFFF -> 0xFFFF
+// set_get_length(uint256): 0xFFF -> 0xFFF
+// set_get_length(uint256): 0xFFFF -> FAILURE # Out-of-gas #
